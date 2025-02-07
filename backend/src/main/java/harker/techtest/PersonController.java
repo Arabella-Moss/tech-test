a0f8fdcee88776fb92a6c187e3286fe01ed544ef
Arabella Moss <ellamoss@protonmail.com>
2025-02-07 18:12:46 +0000

    updated details of person

--- backend/src/main/java/harker/techtest/PersonController.java
+++ backend/src/main/java/harker/techtest/PersonController.java
@@ -21,21 +21,32 @@ public class PersonController {
     private final String postPath = "people";
     private final String putpath = "/people{id}"
 
+
+    // retrieves list of all people from database
     @GetMapping(getPath)
     public List<Person> getPeople() {
         return personService.getAllPeople();
     }
 
+ 
+    // retrieves list of a specific person using unique ID
     @GetMapping(getPath + "/{id}")
     public Person getPerson(@PathVariable("id") String id) {
         return personService.getPerson(id);
     }
 
+
+    // creates a new person in database 
     @PostMapping(postPath)
     public Person postPerson(@RequestBody Person personModel) {
         return personService.postPerson(personModel);
     }
 
-    // Task 3
+    // Task 3, updates details of an existing person identified by their ID
+    @PutMapping(putPath)
+    public person updatePerson(@PathVariable("id") String id, @RequestBody Person personMode1) {
+        return personService.updatePerson(id, personMode1);
+    }
+
     
 }
