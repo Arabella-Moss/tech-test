86a120f6346ee21267f16f7491c3f9581ddb584b
Arabella Moss <ellamoss@protonmail.com>
2025-02-07 18:00:59 +0000

    save changes

--- backend/src/main/java/harker/techtest/PersonService.java
+++ backend/src/main/java/harker/techtest/PersonService.java
@@ -24,8 +24,25 @@ public class PersonService {
         return personRepository.findById(id).orElseThrow(() -> new RuntimeException("Value not present"));
     }
 
+    // method to save a new person to the database 
     public Person postPerson(Person person) {
         return personRepository.save(person);
+    }
+    
+     // method to update an existing person's details
+     public Person updatePerson(String id, Person person) {
+        // check if the person exists in the database, if not throw an error message
+        Person existingPerson = personRepository.findById(id).orElseThrow(() -> new RuntimeException("Person not found with id: " + id));
+
+        // updates details of existing person in database
+        existingPerson.setFirstName(person.getFirstName()); // takes first name from person object, sets it as first name of existingperson object
+        existingPerson.setLastName(person.getLastName());
+        existingPerson.setDateOfBirth(person.getDateOfBirth());
+        existingPerson.setEmail(person.getEmail());
+
+        // saves the updated person back to the repository
+        return personRepository.save(existingPerson);
+
     }
     
 }
