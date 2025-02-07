7d58ede528a2e46c691df39a5b4cf93457dc188c
Arabella Moss <ellamoss@protonmail.com>
2025-02-07 17:58:04 +0000

    implemented post request

--- backend/src/main/java/harker/techtest/PersonService.java
+++ backend/src/main/java/harker/techtest/PersonService.java
@@ -12,22 +12,20 @@ public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
+    // retrieves all person records from database
     public List<Person> getAllPeople() {
-        List<Person> people = new ArrayList<>();
-
-        personRepository.findAll().forEach(people::add);
-
-        return people;
+        List<Person> people = new ArrayList<>(); // creates a new empty list to store the records
+        personRepository.findAll().forEach(people::add); // fetches records, adds each person to new list 
+        return people; //returns list of people
     }
 
+        // method to fetch specific person based on their ID, if not throw error message
     public Person getPerson(String id) {
         return personRepository.findById(id).orElseThrow(() -> new RuntimeException("Value not present"));
     }
 
     public Person postPerson(Person person) {
-        // Task 2
-
-        return person;
+        return personRepository.save(person);
     }
     
 }
